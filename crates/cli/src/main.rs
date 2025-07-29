87a7b528482a1850731001ee8ccaccfd75c64824
Miquel Sabaté Solà <mikisabate@gmail.com>
2025-07-29 08:12:20 +0200

    Rename from run to practice
    Signed-off-by: Miquel Sabaté Solà <mikisabate@gmail.com>

--- crates/cli/src/main.rs
+++ crates/cli/src/main.rs
@@ -17,7 +17,7 @@ fn help() {
     println!("Commands:");
     println!("   init\t\t\tInitialize the configuration for this application.");
     println!("   nuke\t\t\tRemove all files from this application and its database.");
-    println!("   run\t\t\tRun exercises. Default command if none was given.");
+    println!("   practice\t\tPractice vocabulary/exercises. Default command if none was given.");
     println!("   words\t\tManage the words for this application.");
 }
 
@@ -29,44 +29,42 @@ fn main() {
     args.next();
 
     match args.next() {
-        Some(command_flag) => {
-            match command_flag.as_str() {
-                "-h" | "--help" => {
-                    if nargs > 2 {
-                        println!("warning: arguments passed the 'help' flag will be ignored.\n");
-                    }
-                    help();
-                    std::process::exit(0);
-                },
-                "-v" | "--version" => {
-                    if nargs > 2 {
-                        println!("warning: arguments passed the 'version' flag will be ignored.\n");
-                    }
-                    println!("mihi {VERSION}");
-                    std::process::exit(0);
-                },
-                "init" => {
-                    let rest: Vec<String> = args.collect();
-                    init::run(rest);
-                },
-                "nuke" => {
-                    let rest: Vec<String> = args.collect();
-                    nuke::run(rest);
-                },
-                "words" => {
-                    let rest: Vec<String> = args.collect();
-                    words::run(rest);
-                },
-                "run" => {
-                    let rest: Vec<String> = args.collect();
-                    run::run(rest);
-                },
-                _ => {
-                    println!("error: unknown flag or command: '{command_flag}'");
-                    std::process::exit(1);
+        Some(command_flag) => match command_flag.as_str() {
+            "-h" | "--help" => {
+                if nargs > 2 {
+                    println!("warning: arguments passed the 'help' flag will be ignored.\n");
                 }
+                help();
+                std::process::exit(0);
+            }
+            "-v" | "--version" => {
+                if nargs > 2 {
+                    println!("warning: arguments passed the 'version' flag will be ignored.\n");
+                }
+                println!("mihi {VERSION}");
+                std::process::exit(0);
+            }
+            "init" => {
+                let rest: Vec<String> = args.collect();
+                init::run(rest);
+            }
+            "nuke" => {
+                let rest: Vec<String> = args.collect();
+                nuke::run(rest);
+            }
+            "words" => {
+                let rest: Vec<String> = args.collect();
+                words::run(rest);
+            }
+            "practice" => {
+                let rest: Vec<String> = args.collect();
+                run::run(rest);
+            }
+            _ => {
+                println!("error: unknown flag or command: '{command_flag}'");
+                std::process::exit(1);
             }
         },
-        None => run::run(Vec::new())
+        None => run::run(Vec::new()),
     }
 }
